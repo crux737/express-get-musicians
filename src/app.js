cd1da791a0c361e31559c4f95af1a1593e94660e
Gboyega Idowu <gboyegaidowu@Gboyegas-MBP.broadband>
2023-11-06 15:14:30 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,9 +6,15 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+app.get("/musicians", async (req, res) => {
+    const musicians = await Musician.findAll({});
+    res.json(musicians)
+});
 
-
-
+app.get("/musicians/1", async (req, res) => {
+    const musicianOne = await Musician.findByPk(1);
+    res.json(musicianOne)
+});
 
 
 
